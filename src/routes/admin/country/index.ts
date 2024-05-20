e5c9f83b1a6698ce7cc18dcd0bdf82452c295ad4
sharmajiatsukrit <sagar@sukritinfotech.com>
2024-05-20 16:34:51 +0530

    fix country routes

--- src/routes/admin/country/index.ts
+++ src/routes/admin/country/index.ts
@@ -5,11 +5,11 @@ import { authAdmin, validateRequest } from "../../../utils/middleware";
 const routes: Router = expres.Router();
 const countryController = new CountryController();
 
-routes.get("/", authAdmin, countryController.getList.bind(countryController));
+routes.get("/list", validateRequest, countryController.getList.bind(countryController));
 routes.post("/add", validateRequest, countryController.add.bind(countryController));
-routes.put("/update/:id", authAdmin, countryController.update.bind(countryController));
-routes.get("/by-id/:id", authAdmin, countryController.getDetailsById.bind(countryController));
-routes.delete("/delete/:id", authAdmin, countryController.delete.bind(countryController));
-routes.patch("/status/:id", authAdmin, countryController.status.bind(countryController));
+routes.put("/update/:id", validateRequest, countryController.update.bind(countryController));
+routes.get("/by-id/:id", validateRequest, countryController.getDetailsById.bind(countryController));
+routes.delete("/delete/:id", validateRequest, countryController.delete.bind(countryController));
+routes.patch("/status/:id", validateRequest, countryController.status.bind(countryController));
 
 export default routes;
